b53e07b44affda47a4ba1b542ab1c49d6b85874f
immasoud <49973074+immasoud@users.noreply.github.com>
2021-10-15 00:14:47 +0330

    Part 1 Done, Theory getVisits Done

--- OwnerTest.java
+++ OwnerTest.java
@@ -2,12 +2,15 @@ package org.springframework.samples.petclinic.owner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.cucumber.messages.internal.com.google.common.collect.Lists;
 import org.junit.*;
+import org.junit.experimental.theories.Theory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
 
 import static  org.junit.Assert.*;
 import java.util.*;
@@ -15,27 +18,50 @@ import java.util.*;
 class OwnerTest {
 
 	private List<Pet> petList;
-	private String telephone;
 	private Owner owner;
 	private Pet pet;
+	private List<String> petNameList;
+	private int numberOfPets;
 
 	public void creatPetList(int numberOfPets)
 	{
 		for(int i = 0; i < numberOfPets; i++)	{
 			Pet pet = new Pet();
-			this.petList.add(pet);
-			this.owner.addPet(pet);
+			petList.add(pet);
+			owner.addPet(pet);
+		}
+	}
+
+	public void nameSetter(List<String> petNameList, int numberOfPets)
+	{
+		for(int i = 0; i < numberOfPets; i++)	{
+			petList.get(i).setName(petNameList.get(i));
+		}
+	}
+
+	public void idSetter(int numberOfPets)
+	{
+		for(int i = 0; i < numberOfPets; i++)	{
+			petList.get(i).setId(i);
 		}
 	}
 
 	@BeforeEach
 	public void setUp()
 	{
-		this.petList = new ArrayList<Pet>();
-		this.owner = new Owner();
-		this.pet = new Pet();
+		petList = new ArrayList<Pet>();
+		owner = new Owner();
+		pet = new Pet();
+		petNameList = new ArrayList<String>();
+		numberOfPets = 3;
 
-		System.out.println("Before");
+		for (int i = 0; i < numberOfPets; i++)
+		{
+			String temp = "IN" + Integer.toString(i);
+			petNameList.add(temp);
+		}
+
+		System.out.println("Set up");
 	}
 
 	@DisplayName("addPet")
@@ -43,10 +69,12 @@ class OwnerTest {
 	public void TestAddPet()
 	{
 		//Execution
-		this.petList.add(pet);
-		this.owner.addPet(pet);
+		petList.add(pet);
+		owner.addPet(pet);
 		//Result Verification
-		Assertions.assertEquals(petList, owner.getPets());
+		Set<Pet> petSet = new HashSet<Pet>(petList);
+		Assertions.assertEquals(petSet, owner.getPetsInternal());
+
 		System.out.println("Test 1");
 	}
 
@@ -55,13 +83,14 @@ class OwnerTest {
 	public void TestRemovePetPositive()
 	{
 		//Delegated Setup
-		creatPetList(3);
+		creatPetList(numberOfPets);
 		//Execution
-		Pet pet = this.petList.get(1);
-		this.petList.remove(pet);
-		this.owner.removePet(pet);
+		Pet pet = petList.get(1);
+		petList.remove(pet);
+		owner.removePet(pet);
 		//Result Verification
-		Assertions.assertEquals(petList, owner.getPets());
+		Set<Pet> petSet = new HashSet<Pet>(petList);
+		Assertions.assertEquals(petSet, owner.getPetsInternal());
 
 		System.out.println("Test 2");
 	}
@@ -70,51 +99,108 @@ class OwnerTest {
 	@Test
 	public void TestRemovePetNegative()
 	{
-		creatPetList(3);
+		//Delegated Setup
+		creatPetList(numberOfPets);
+		//Execution
+		owner.removePet(pet);
+		//Result Verification
+		Set<Pet> petSet = new HashSet<Pet>(petList);
+		Assertions.assertEquals(petSet, owner.getPetsInternal());
 
-		this.owner.removePet(pet);
+		System.out.println("Test 3");
 
-
-		System.out.println("Test 2");
 	}
 
-
-	@DisplayName("getPet Positive,ignoreNew = false")
+	@DisplayName("getPet Positive, ignoreNew = false")
 	@Test
 	public void TestGetPetPositive()
 	{
-		creatPetList(3);
+		creatPetList(numberOfPets);
+		nameSetter(petNameList, numberOfPets);
 
-		this.petList.get(0).setName("Hoshang");
-		this.petList.get(1).setName("Pishtol");
-		this.petList.get(2).setName("Soltan");
+		Pet resultPet = owner.getPet("IN1", false);
 
-		Assertions.assertEquals(this.petList.get(1), owner.getPet("pishtol", false));
+		Assertions.assertEquals(petList.get(1), resultPet);
 
-		System.out.println("Test 2");
+		System.out.println("Test 4");
 	}
 
 	@DisplayName("getPet Negative, ignoreNew = false")
 	@Test
-	public void TestGetPetNegative()
+	public void TestGetPetNegative1()
 	{
-		creatPetList(3);
+		creatPetList(numberOfPets);
+		nameSetter(petNameList, numberOfPets);
 
-		this.petList.get(0).setName("Hoshang");
-		this.petList.get(1).setName("Pishtol");
-		this.petList.get(2).setName("Soltan");
+		Pet resultPet = owner.getPet("Not-IN", false);
 
-		Assertions.assertEquals(null, owner.getPet("shayat", false));
+		Assertions.assertEquals(null, resultPet);
 
-		System.out.println("Test 2");
+		System.out.println("Test 5");
+	}
+
+	@DisplayName("getPet Negative, ignoreNew = true, Set ID")
+	@Test
+	public void TestGetPetNegative2()
+	{
+		// Set up
+		creatPetList(numberOfPets);
+		nameSetter(petNameList, numberOfPets);
+		idSetter(numberOfPets);
+
+		Pet resultPet =  owner.getPet("IN1", true);
+
+		Assertions.assertEquals(petList.get(1),resultPet);
+
+		System.out.println("Test 6");
+	}
+
+	@DisplayName("getPet Negative, ignoreNew = true, Set ID")
+	@Test
+	public void TestGetPetPositive2()
+	{
+		//Set up
+		creatPetList(numberOfPets);
+		nameSetter(petNameList, numberOfPets);
+		idSetter(numberOfPets);
+
+		Pet resultPet = owner.getPet("Not-IN", true);
+
+		Assertions.assertEquals(null, resultPet);
+
+		System.out.println("Test 7");
+	}
+
+	@DisplayName("getPets")
+	@Test
+	public void TestGetPets()
+	{
+
+		creatPetList(numberOfPets);
+		List<String> rev = Lists.reverse(petNameList);
+		nameSetter(rev, numberOfPets);
+		petList = Lists.reverse(petList);
+
+		List<Pet> resultPetList = owner.getPets();
+
+		Assertions.assertEquals(petList, resultPetList);
+
+		System.out.println("Test 8");
 	}
 
 	@AfterEach
 	public void tearDown()
 	{
-		this.petList = null;
-		this.owner = null;
-		this.pet = null;
-		System.out.println("After");
+		petList = null;
+		owner = null;
+		pet = null;
+		petNameList = null;
+
+		System.out.println("Tear Down");
 	}
 }
+//@RunWith(Theory.class)
+//class OwnerTestTheory
+//{
+//
+//}
